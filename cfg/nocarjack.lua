948e2ca959d839d28f86d7054bdd59700433e6ac
renildomarcio <krsolucoesweb@gmail.com>
2019-06-12 12:49:48 -0300

    Update v1.0 @ESX-Brasil

---
+++ cfg/nocarjack.lua
@@ -0,0 +1,28 @@
+cfg = {}
+
+cfg.chance = 5 -- chance de ser desbloqueado em porcentagem
+
+cfg.blacklist = { -- veículos que sempre serão bloqueados quando gerados naturalmente
+  "T20",
+  "police",
+  "police2",
+  "police3",
+  "sheriff",
+  "sheriff3",
+  "sheriff2",
+  "riot",
+  "fbi",
+  "hwaycar2",
+  "hwaycar3",
+  "hwaycar10",
+  "hwaycar",
+  "polf430",
+  "policeb",
+  "police7",
+  "RHINO"
+}
+
+cfg.job_whitelist = {
+  "police",
+  "ambulance"
+}
